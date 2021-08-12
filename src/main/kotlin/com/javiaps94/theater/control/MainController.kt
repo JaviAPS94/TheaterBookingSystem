1fb7de2cface43640a2297f72cfa5fa77770057e
Alex Pinaida <alex.pinaida@rappi.com>
2021-08-11 22:10:14 -0500

    feat: initialize spring project and added hellor world controller

---
+++ src/main/kotlin/com/javiaps94/theater/control/MainController.kt
@@ -0,0 +1,14 @@
+package com.javiaps94.theater.control
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+class MainController {
+
+    @RequestMapping("helloWorld")
+    fun helloWorld() : ModelAndView {
+        return ModelAndView("helloWorld")
+    }
+}
---
+++ src/test/kotlin/com/javiaps94/theater/TheaterBookingSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.javiaps94.theater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheaterBookingSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
