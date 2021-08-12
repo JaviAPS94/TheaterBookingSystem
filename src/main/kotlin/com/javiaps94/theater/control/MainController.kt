71d6e7b4ae81a4024ba89f1835f3e7bb98cdaf31
Alex Pinaida <alex.pinaida@rappi.com>
2021-08-11 23:10:37 -0500

    feat: posting data to a controller function

--- src/main/kotlin/com/javiaps94/theater/control/MainController.kt
+++ src/main/kotlin/com/javiaps94/theater/control/MainController.kt
@@ -2,6 +2,7 @@ package com.javiaps94.theater.control
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
@@ -9,6 +10,11 @@ class MainController {
     @RequestMapping("")
     fun homePage() : ModelAndView =
         ModelAndView("seatBooking", "bean", CheckAvailabilityBackingBean())
+
+    @RequestMapping("checkAvailability", method = [RequestMethod.POST])
+    fun checkAvailability() {
+
+    }
 }
 
 
